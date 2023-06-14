722c69279a57f114f4b4d93bf6d43a6a64526d2b
Blake Hua <blake@Blakes-MacBook-Pro.local>
2023-06-14 00:02:45 -0700

    final commit

--- final-project/script.sh
+++ final-project/script.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
-# Run Terraform apply
+# Run Terraform apply, -auto-approve makes it so that we don't have to type 'yes'
 terraform apply -auto-approve
 
 # Check if Terraform apply was successful
 if [ $? -eq 0 ]; then
+
   # Get the public IP address of the instance
   instance_ip=$(terraform output -raw instance_ip_addr)
 
+  # Get the key pair name from Terraform state
+  echo "Retrieving key pair name from Terraform state..."
+  key_pair_name=$(terraform show -json | jq -r '.values.root_module.resources[] | select(.type=="aws_key_pair" and .name=="generated_key") | .values.key_name')
+  echo "Key pair name: $key_pair_name"
+
+  # Use the key pair name as the private key filename
+  private_key_file="$key_pair_name"
+
+  # Get the key pair from Terraform state
+  echo "Retrieving key pair from Terraform state..."
+  key_pair=$(terraform show -json | jq -r '.values.root_module.resources[] | select(.type=="aws_key_pair" and .name=="generated_key") | .values.key_pair_id')
+  echo "Key pair ID: $key_pair"
+
+  # Set appropriate permissions for the private key file
+  chmod 400 $private_key_file
+
   # SSH into the instance/ubuntu machine
-  ssh -i /Users/blake/Downloads/mc-key2.pem ubuntu@$instance_ip '
+  ssh -i $private_key_file.pem ubuntu@$instance_ip '
   sudo apt update 
   sudo apt upgrade  
   sudo add-apt-repository ppa:linuxuprising/java 
@@ -23,12 +40,7 @@ if [ $? -eq 0 ]; then
   java -Xmx1024M -Xms1024M -jar server.jar nogui
   '
   
-  # Additional commands or actions after creating the folder
-  # ...
-
   echo "Commands executed successfully!"
 else
-  echo "Terraform apply failed. Folder creation aborted."
+  echo "Terraform apply failed."
 fi
-
-echo "Instance IP: $instance_ip"
