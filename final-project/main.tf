d09b325cb17d88a99e2e3bd6f5b91b909bafa125
Blake Hua <blake@Blakes-MacBook-Pro.local>
2023-06-14 13:54:45 -0700

    final commit

--- final-project/main.tf
+++ final-project/main.tf
@@ -14,40 +14,56 @@ provider "aws" {
   profile = "default"
 }
 
-/*resource "aws_security_group" "minecraft" {
+# Security Groups
+resource "aws_security_group" "minecraft-security" {
+  name        = "minecraft-security"
+  description = "Allow inbound traffic on port 25565 and SSH on port 22"
+
   ingress {
-    description = "Minecraft port"
     from_port   = 25565
     to_port     = 25565
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
-    description = "Send Anywhere"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  tags = {
-    Name = "Minecraft Security Group"
-  }
-}*/
 
-# Network settings
+  tags = {
+    Name = "minecraft security group"
+  }
+}
+
+# Creates Key Pair
+resource "aws_key_pair" "generated_key" {
+  key_name   = "var.ssh_key_name"
+  public_key = "var.public_key"
+}
+
+# Create Instance
 resource "aws_instance" "minecraft" {
   ami                         = "ami-03f65b8614a860c29"
   instance_type               = "t3.medium"
-  vpc_security_group_ids      = ["sg-09495713dec44541c"]
-  subnet_id                   = "subnet-0be6db793e48c3326"
+  vpc_security_group_ids      = [aws_security_group.minecraft-security.id]
   associate_public_ip_address = true
-  key_name                    = "mc-key2"
+  key_name                    = aws_key_pair.generated_key.key_name
   tags = {
     Name = "Minecraft Server"
   }
 }
 
-
+# Outputs Public IP Address
 output "instance_ip_addr" {
   value = aws_instance.minecraft.public_ip
 }
